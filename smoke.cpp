d53a2eb7faaa729aa82a136cd7a9393dfce8aa53
linux1mint1 <olilechnik@gmail.com>
2022-07-17 19:39:17 +0200

    finished update function in Smoke class. minor adjustments in Particle struct

--- smoke.cpp
+++ smoke.cpp
@@ -1,22 +1,19 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
-#include <cmath>
-#include <vector>
-
-const float DEG_TO_RAD =  M_PI/180;
+#include "utensils.cpp"
 
 class Smoke {
 
     struct Particle {
 
         // some constants for easy editon in the future
-        const float airFriction = 0.03;
-        const float upwardForce = 0;
-        const float angularFriction = 0.03;
-        const int angleRandomness = 45; // max deviation in each direction. [degrees] 
-        const int sizeRandomness = 50; // +- max deviation in size. [percentage]
-        const int speedRandomness = 30; // +- max deviation in speed. [percentage]
+        constexpr static const float airFriction = 0.03;
+        constexpr static const float upwardForce = 0;
+        constexpr static const float angularFriction = 0.03;
+        constexpr static const int angleRandomness = 45; // max deviation in each direction. [degrees] 
+        constexpr static const int sizeRandomness = 50; // +- max deviation in size. [percentage]
+        constexpr static const int speedRandomness = 30; // +- max deviation in speed. [percentage]
+        constexpr static const float maxLifetime = 3;
 
         sf::Vector2f pos; // position
         sf::Vector2f vel; // velocity
@@ -27,11 +24,11 @@ class Smoke {
         float rotation;
 
         Particle (
-                    sf::Vector2f startPos,
-                    float speed,
-                    float angle,
-                    float size,
-                    float dTime = 0.f // for more continous movement (not nececery)
+                sf::Vector2f startPos,
+                float speed,
+                float angle,
+                float size,
+                float dTime = 0.f // for more continous movement (not nececery)
             ) {
 
             // randomise input
@@ -61,6 +58,10 @@ class Smoke {
             this->update(rand()%(int)(dTime*100)/100.f);
         }
 
+        float getLifetimeRatio ( ) {
+            return lifetime / maxLifetime;
+        }
+
         void update (
             float dTime
             ) {
@@ -78,12 +79,73 @@ class Smoke {
 
     };
 
+    std::vector <Particle> particles;
+    sf::Sprite sprite; // partiles dont have own sprite. 1 sprite will be used to draw all of them
+    sf::Texture txt;
+    sf::Color color; // white by default
+
 public:
 
-    Smoke (
+    Smoke ( ) {
+    }
 
+    void update(
+            float dTime,
+            sf::Vector2f pos,
+            float direction,
+            float density, // (averege) smoke particles per second
+            float speed
         ) {
+
+            // #1 update all particles (if particle lifetime is higher than max lifetime. kill particle)
+            // #2 decide how much new particles to make
+            // #3 make those particles
+
+        // #1
+        // for(std::vector<Particle>::iterator particle = particles.begin(); particle != particles.end(); ){
+        //     if(particle->lifetime >= maxLifetime){
+        //         particle = particles.erase(particle);
+        //     } else {
+        //         particle->update(dTime);
+        //         particle++;
+        //     }
+        // }
+
+        remove_erase_if(particles, [](Particle& ptr)->bool{
+            if(ptr.getLifetimeRatio() >= 1){
+                return true;
+            }
+            return false;
+        });
         
+        // #2
+        float nptm = dTime * density; // amount of new particles to make
+        int confident = floor(nptm);
+        float poap = nptm - confident; // propability of aditional particle
+        
+        if(rand()%1000/1000.f <= poap) confident += 1;
+
+        // #3
+        for(int i = 0; i < confident; i++){
+            particles.emplace_back(
+                pos,
+                speed,
+                direction,
+                0.1,
+                dTime
+            );
+        }
+
+    }
+
+    virtual void draw(
+            sf::RenderTarget& target,
+            sf::RenderStates states
+        ) const {
+
+        sf::CircleShape crl(50);
+
+        target.draw(crl,states);
     }
 
 };
